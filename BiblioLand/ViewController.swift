1f357cbc4ce19975cc8728d4246a63047da444da
Toriq Wahid Syaefullah <toriqwahidsyaef@gmail.com>
2020-07-17 09:19:13 +0700

    Initial Commit

---
+++ BiblioLand/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BiblioLand
+//
+//  Created by Toriq Wahid Syaefullah on 17/07/20.
+//  Copyright © 2020 Toriq Wahid Syaefullah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
