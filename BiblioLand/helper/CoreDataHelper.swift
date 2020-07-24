7039e55df0b83d3eaf2108af7529010059208af0
Toriq Wahid Syaefullah <toriqwahidsyaef@gmail.com>
2020-07-24 10:37:26 +0700

    core data integration

---
+++ BiblioLand/helper/CoreDataHelper.swift
@@ -0,0 +1,58 @@
+//
+//  CoreDataHelper.swift
+//  BiblioLand
+//
+//  Created by Toriq Wahid Syaefullah on 22/07/20.
+//  Copyright © 2020 Toriq Wahid Syaefullah. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+struct CoreDataHelper {
+    static let context: NSManagedObjectContext = {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            fatalError()
+        }
+
+        let persistentContainer = appDelegate.persistentContainer
+        let context = persistentContainer.viewContext
+
+        return context
+    }()
+    
+    static func newUser() -> User {
+            let user = NSEntityDescription.insertNewObject(forEntityName: "User", into: context) as! User
+
+            return user
+    }
+    
+    static func saveUser() {
+        do {
+            try context.save()
+        } catch let error {
+            print("Could not save \(error.localizedDescription)")
+        }
+    }
+    
+    static func deleteUser(user: User) {
+        context.delete(user)
+
+        saveUser()
+    }
+    
+    static func retrieveUser() -> [User] {
+    do {
+        let fetchRequest = NSFetchRequest<User>(entityName: "User")
+        let results = try context.fetch(fetchRequest)
+
+        return results
+    } catch let error {
+        print("Could not fetch \(error.localizedDescription)")
+
+        return []
+    }
+
+}
+}
---
+++ Book+CoreDataProperties.swift
@@ -0,0 +1,35 @@
+//
+//  Book+CoreDataProperties.swift
+//  BiblioLand
+//
+//  Created by Toriq Wahid Syaefullah on 22/07/20.
+//  Copyright © 2020 Toriq Wahid Syaefullah. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Book {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Book> {
+        return NSFetchRequest<Book>(entityName: "Book")
+    }
+
+    @NSManaged public var author: String?
+    @NSManaged public var borrower_count: Int16
+    @NSManaged public var borrower_name: String?
+    @NSManaged public var category: String?
+    @NSManaged public var condition: String?
+    @NSManaged public var is_borrowed: Bool
+    @NSManaged public var language: String?
+    @NSManaged public var lender_name: String?
+    @NSManaged public var lender_notes: String?
+    @NSManaged public var name: String?
+    @NSManaged public var pages: Int16
+    @NSManaged public var photo: String?
+    @NSManaged public var price_deposit: Int64
+    @NSManaged public var price_per_day: Int64
+
+}
