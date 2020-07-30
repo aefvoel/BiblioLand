5b0106dbc6966ba2b967fb9cd9160e45e428e106
Toriq Wahid Syaefullah <toriqwahidsyaef@gmail.com>
2020-07-30 10:15:11 +0700

    added return book

---
+++ BiblioLand/CurrentBorrowing/Cell/BookBorrowing.swift
@@ -0,0 +1,24 @@
+//
+//  BookBorrowing.swift
+//  BiblioLand
+//
+//  Created by Toriq Wahid Syaefullah on 29/07/20.
+//  Copyright © 2020 Toriq Wahid Syaefullah. All rights reserved.
+//
+
+import UIKit
+
+class BookBorrowing: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
