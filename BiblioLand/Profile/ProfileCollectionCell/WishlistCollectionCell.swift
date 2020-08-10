1c0fc5010899e73eafce4f50bb5ea7fc9db8d9cd
Ajeng <salicca.rahajeng@gmail.com>
2020-08-10 10:59:52 +0700

    profile commit

---
+++ BiblioLand/Profile/ProfileCollectionCell/WishlistCollectionCell.swift
@@ -0,0 +1,34 @@
+//
+//  WishlistCollectionCell.swift
+//  BiblioLand
+//
+//  Created by Salicca Dewi Rahajeng on 06/08/20.
+//  Copyright © 2020 Toriq Wahid Syaefullah. All rights reserved.
+//
+
+import UIKit
+
+class WishlistCollectionCell: UICollectionViewCell {
+
+    @IBOutlet weak var bookImg: UIImageView!
+    @IBOutlet weak var bookTitleLbl: UILabel!
+    @IBOutlet weak var bgView: UIView!
+    
+    static let identifier = "WishlistCollectionCell"
+    static func nib() -> UINib{
+        return UINib(nibName: "WishlistCollectionCell", bundle: nil)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        bgView.layer.cornerRadius = 7.0
+    }
+    
+    public func configure(with bookWishlist: BookWishlist){
+        self.bookTitleLbl.text = bookWishlist.bookTitle
+        self.bookImg.image = bookWishlist.bookImg
+    }
+
+}
