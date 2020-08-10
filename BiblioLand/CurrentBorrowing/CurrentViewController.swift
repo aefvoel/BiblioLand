18a00b8b3b079c3a8d7af9e373ea2edefddec958
Calvin Leonardo <calvin.leon19@gmail.com>
2020-08-10 11:25:32 +0700

    Remove Scroll indicator

--- BiblioLand/CurrentBorrowing/CurrentViewController.swift
+++ BiblioLand/CurrentBorrowing/CurrentViewController.swift
@@ -33,6 +33,7 @@ class CurrentViewController: UIViewController {
         
         listCurrentBook.delegate = self
         listCurrentBook.dataSource = self
+    
         
 //        tableView.register(UINib(nibName: "BookBorrowing", bundle: nil), forCellReuseIdentifier: "BookBorrowing")
 
@@ -83,6 +84,8 @@ extension CurrentViewController: UITableViewDelegate, UITableViewDataSource {
             cell2.booksTitle.text = dataCheckout[indexPath.section].booksData?[indexPath.row].bookTitle
             cell2.borrowerNAme.text = dataCheckout[indexPath.section].borrowers?.boorowersName
             
+            cell2.selectionStyle = .none
+
             return cell2
     }
     
@@ -92,7 +95,7 @@ extension CurrentViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 130
+        return 145
 
     }
 }
